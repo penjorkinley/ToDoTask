58ffd152b08b359ca3272249d6540b5d816b2476
popss <pemalw99@gmail.com>
2022-09-21 16:37:26 +0530

    added mother's information activity

---
+++ app/src/main/java/com/example/motherchild_digitalhealth_book/MothersInformation.java
@@ -0,0 +1,14 @@
+package com.example.motherchild_digitalhealth_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MothersInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mothers_information);
+    }
+}
