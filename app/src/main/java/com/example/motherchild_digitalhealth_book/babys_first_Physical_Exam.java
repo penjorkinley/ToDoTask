20b479f9aea24b467bb9e2de410e4e022010aa8d
karzeydema <110452208+karzeydema@users.noreply.github.com>
2022-10-02 17:22:52 +0600

    added babys physical exam

---
+++ app/src/main/java/com/example/motherchild_digitalhealth_book/babys_first_Physical_Exam.java
@@ -0,0 +1,14 @@
+package com.example.motherchild_digitalhealth_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class babys_first_Physical_Exam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_babys_first_physical_exam);
+    }
+}
