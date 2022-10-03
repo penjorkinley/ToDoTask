884dbe74a60e975ffc267f78197c2e23b642f7d7
karzeydema <110452208+karzeydema@users.noreply.github.com>
2022-10-03 14:48:47 +0600

    added health care info design

---
+++ app/src/main/java/com/example/motherchild_digitalhealth_book/HeathWorkers_Info.java
@@ -0,0 +1,14 @@
+package com.example.motherchild_digitalhealth_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeathWorkers_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heath_workers_info);
+    }
+}
